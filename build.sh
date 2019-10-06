8e94d779cae41c64c4f294e37bbc0f1be524ea3b
Chensienyong <ashxcellent_kingzr@yahoo.com>
2019-10-06 22:02:16 +0700

    hmm

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 curl -L -O https://github.com/gohugoio/hugo/releases/download/v0.56.2/hugo_extended_0.56.2_Linux-64bit.tar.gz
 tar -xzf hugo_extended_0.56.2_Linux-64bit.tar.gz
 
-hugo server
+./hugo -v
